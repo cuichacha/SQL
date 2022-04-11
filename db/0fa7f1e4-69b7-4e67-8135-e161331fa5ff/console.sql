3ed4124037c0206cc9a82416393cc7f34e43c020
Will Dufresne <chxdtc123@gmail.com>
2022-04-11 15:06:08 +0800

    Commit

---
+++ db/0fa7f1e4-69b7-4e67-8135-e161331fa5ff/console.sql
@@ -0,0 +1,30 @@
+use dsmzg_db;
+
+select *
+from uc_message_file
+limit 1;
+
+select file_name
+from uc_message_file
+limit 1000 offset 80000000;
+
+select count(*)
+from uc_message_file;
+
+select file_name
+from uc_message_file where file_name = '09225732351640069515279';
+
+select *
+from uc_message_file where `__#alibaba_rds_row_id#__` = 80000000;
+
+select *
+from uc_message_file where `__#alibaba_rds_row_id#__` = 110000000;
+
+select *
+from uc_message_file where `__#alibaba_rds_row_id#__` = 210000000;
+
+select *
+from uc_message_file where `__#alibaba_rds_row_id#__` = 333999588;
+
+select *
+from uc_message_file where `__#alibaba_rds_row_id#__` = 333999587;
---
+++ db/8bbfcb3a-16ae-4a9c-9707-ba13ca4f7688/console.sql
@@ -0,0 +1,62 @@
+# 繁体字相关开始---------------------------------------------------------------------------------------------------------
+
+# alter table sc_feedback_reason
+#     add language_type tinyint;
+# alter table sc_opinion_feedback
+#     add language_type tinyint;
+#
+# alter table sc_feedback_reason
+#     drop language_type;
+# alter table sc_opinion_feedback
+#     drop language_type;
+
+alter table sc_online_server
+    add language_type tinyint;
+
+# 繁体字相关开始---------------------------------------------------------------------------------------------------------
+
+
+
+select id,
+       create_date,
+       update_date,
+       version,
+       title,
+       title_url,
+       title_content,
+       useful,
+       useless,
+       parent_id,
+       level,
+       status,
+       display,
+       operator_id,
+       language_type
+from sc_online_server
+WHERE level = 2
+  and display = 0
+  and language_type = 2
+order by id desc;
+
+select *
+from sc_co_order
+where shop_name = '京东自营'
+  and status = 1301
+  and order_type = 110
+  and waybill_status = 0
+  and (common_label & 32768) = 32768
+order by paid_date desc;
+
+select *
+from sc_co_order where (common_label & 32768) = 32768;
+
+select seller_memo
+from sc_co_order where id = 491724042144579584;
+
+select seller_memo
+from sc_co_order where id = 520172694915514369;
+
+select *
+from sc_co_order where id = 492006900448886784;
+
+update sc_co_order set seller_memo = ((common_label & 32768) = 32768) where id = 520172694915514369;
