5c693d570bf5e4d25fca665f8084459392161457
Will Dufresne <chxdtc123@gmail.com>
2022-05-05 13:07:41 +0800

    Commit

--- db/1cd69c49-ec1c-4fa3-bd6b-850499cfa5cb/apollo_SQL.sql
+++ db/1cd69c49-ec1c-4fa3-bd6b-850499cfa5cb/apollo_SQL.sql
@@ -1,5 +1,3 @@
-
-
 select id,
        create_date,
        update_date,
@@ -31,15 +29,28 @@ where shop_name = '京东自营'
 order by paid_date desc;
 
 select *
-from sc_co_order where (common_label & 32768) = 32768;
+from sc_co_order
+where (common_label & 32768) = 32768;
 
 select seller_memo
-from sc_co_order where id = 491724042144579584;
+from sc_co_order
+where id = 491724042144579584;
 
 select seller_memo
-from sc_co_order where id = 520172694915514369;
+from sc_co_order
+where id = 520172694915514369;
 
 select *
-from sc_co_order where id = 492006900448886784;
+from sc_co_order
+where id = 492006900448886784;
 
-update sc_co_order set seller_memo = ((common_label & 32768) = 32768) where id = 520172694915514369;
+update sc_co_order
+set seller_memo = ((common_label & 32768) = 32768)
+where id = 520172694915514369;
+
+insert into sc_online_server (create_date, update_date, version, title, title_url, title_content, useful, useless,
+                              parent_id, level, status, display, operator_id)
+select create_date, update_date, version, title, title_url, title_content, useful, useless, parent_id, level, status,
+        display, operator_id
+from sc_online_server
+where id = 396;
